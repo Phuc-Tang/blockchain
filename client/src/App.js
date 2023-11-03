ba4f5d8440e925b1b1bdf16e7b882e9443987ba5
lenguyencamtien <98004346+lenguyencamtien@users.noreply.github.com>
2023-11-03 20:41:58 +0700

    TIEN: tien

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div> HIIIIII TUI LÀ CTIN NE </div>
+        <div> 123 </div>
       </header>
     </div>
   );
